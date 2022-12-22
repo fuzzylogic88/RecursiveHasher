5cbfce3031d90eef0ad2a0c25b46ed22b3e5c1f4
Daniel Green <daniel.green927@outlook.com>
2022-12-21 23:06:11 -0800

    Continued dev.

--- RecursiveHasher/Program.cs
+++ RecursiveHasher/Program.cs
@@ -17,13 +17,39 @@ namespace RecursiveHasher
     internal class Program
     {
         public static bool GoSpin = false;
+        public static string RootDirectory = string.Empty;
 
         [STAThread]
         static void Main(string[] args)
         {
-            Task.Factory.StartNew(() => LoadSpinTask());
-            Console.Title = "Recursive MD5 Hasher";
+            try
+            {
+                Task.Factory.StartNew(() => LoadSpinTask());
+                Console.Title = "Recursive MD5 Hasher / Directory Comparer";
 
+                Console.WriteLine("Press D to select a directory.");
+                Console.WriteLine("Press C to open files for comparison.");
+
+                ConsoleKeyInfo op = Console.ReadKey();
+                switch (op.KeyChar.ToString().ToUpper())
+                {
+                    case "D":
+                        DirectoryAnalysis();
+                        break;
+                    case "C":
+                        ResultComparison();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+        }
+
+        static void DirectoryAnalysis()
+        {
             List<string> files = new List<string>();
             while (!files.Any())
             {
@@ -35,23 +61,29 @@ namespace RecursiveHasher
             }
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            HashFinder(files);
+            if (HashFinder(files) != string.Empty)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Finished in " + stopwatch.Elapsed.ToString() + ".");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Process failed.");
+                Console.ReadKey();
+            }
 
-            GoSpin = false;
-
-            Console.WriteLine("");
-            Console.WriteLine("Finished in " + stopwatch.Elapsed.ToString() + ".");
-            Console.ReadKey();
         }
 
         static List<string> FileList()
         {
-            Console.WriteLine("Select a folder to read...");
+            Console.WriteLine("Select a directory to read...");
 
-            string dir = string.Empty;
+            RootDirectory = string.Empty;
             List<string> files = new List<string>();
 
-            while (dir == string.Empty)
+            while (RootDirectory == string.Empty)
             {
                 FolderBrowserDialog hf = new FolderBrowserDialog()
                 {
@@ -63,8 +95,8 @@ namespace RecursiveHasher
 
                 if (hf.SelectedPath != string.Empty)
                 {
-                    dir = hf.SelectedPath;
-                    Console.WriteLine("Selected path: '" + dir.ToString() + "'");
+                    RootDirectory = hf.SelectedPath;
+                    Console.WriteLine("Selected path: '" + RootDirectory + "'");
                     Console.WriteLine("Reading directory info");
                     GoSpin = true;
                     AddFiles(hf.SelectedPath, files);
@@ -108,74 +140,129 @@ namespace RecursiveHasher
             }
         }
 
-        static void HashFinder(List<string> files)
+        static string HashFinder(List<string> files)
         {
-            Console.WriteLine("\rCalculating file hashes, please wait");
-            string LogPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\hashout.csv";
-
-            decimal currentfile = 1;
-
-            ConcurrentBag<FileData> resultdata = new ConcurrentBag<FileData>();
-
-            FileData objHeader = new FileData()
+            try
             {
-                FilePath = "Directory",
-                FileHash = "MD5 Hash",
-                DateOfAnalysis = "Date analyzed"
-            };
-            resultdata.Add(objHeader);
+                Console.WriteLine("\rCalculating file hashes, please wait");
 
-            // Compute MD5 hashes for each file in selected directory
-            Parallel.ForEach(files, f =>
-                {
-                    FileData fd = new FileData()
-                    {
-                        FilePath = f,
-                        DateOfAnalysis = DateTime.Now.ToString()
-                    };
+                // Create a unique filename for our output file
+                string FolderName = new DirectoryInfo(RootDirectory).Name;
+                string LogPath = FilenameGenerator(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "FileHashes_" + FolderName + ".csv", 1024);
 
-                    using (MD5 MD5hsh = MD5.Create())
+                decimal currentfile = 1;
+
+                ConcurrentBag<FileData> resultdata = new ConcurrentBag<FileData>();
+
+                //FileData objHeader = new FileData()
+                //{
+                //    FilePath = "Directory",
+                //    FileHash = "MD5 Hash",
+                //    DateOfAnalysis = "Date analyzed"
+                //};
+                //resultdata.Add(objHeader);
+
+                // Compute MD5 hashes for each file in selected directory
+                Parallel.ForEach(files, f =>
                     {
-                        try
+                        FileData fd = new FileData()
                         {
-                            using (var stream = File.OpenRead(f))
+                            FilePath = f,
+                            DateOfAnalysis = DateTime.Now.ToString()
+                        };
+
+                        using (MD5 MD5hsh = MD5.Create())
+                        {
+                            try
                             {
-                                Console.WriteLine("\x000DCurrent File: " + f.ToString());
-                                string MD5 = BitConverter.ToString(MD5hsh.ComputeHash(stream)).Replace("-", string.Empty);
+                                using (var stream = File.OpenRead(f))
+                                {
+                                    Console.WriteLine("\rCurrent File: " + f.ToString());
+                                    string MD5 = BitConverter.ToString(MD5hsh.ComputeHash(stream)).Replace("-", string.Empty);
 
-                                fd.FileHash = MD5;
+                                    fd.FileHash = MD5;
+                                    resultdata.Add(fd);
+
+                                    // Post progress to console titlebar
+                                    decimal p = Math.Round(currentfile / files.Count() * 100m, 2);
+                                    Console.Title = p.ToString() + "% complete.";
+                                    currentfile++;
+                                }
+                            }
+                            catch (UnauthorizedAccessException)
+                            {
+                                fd.FileHash = "Read access denied.";
                                 resultdata.Add(fd);
+                            }
 
-                                // Post progress to console titlebar
-                                decimal p = Math.Round(currentfile / files.Count() * 100m, 2);
-                                Console.Title = p.ToString() + "% complete.";
-                                currentfile++;
+                            catch (IOException)
+                            {
+                                fd.FileHash = "File in use.";
+                                resultdata.Add(fd);
                             }
                         }
-                        catch (UnauthorizedAccessException)
-                        {
-                            fd.FileHash = "Read access denied.";
-                            resultdata.Add(fd);
-                        }
+                    });
 
-                        catch (IOException)
-                        {
-                            fd.FileHash = "File in use.";
-                            resultdata.Add(fd);
-                        }
-                    }
-                });
-
-            // Write computed hashes to .csv on desktop
-            Console.WriteLine("\rWriting results to disk");
-            using (var sw = new StreamWriter(LogPath))
-            {
-                using (CsvWriter csv = new CsvWriter(sw, CultureInfo.CurrentCulture))
+                // Write computed hashes to .csv on desktop
+                Console.WriteLine("\rWriting results to disk");
+                using (var sw = new StreamWriter(LogPath))
                 {
-                    csv.WriteRecords(resultdata);
-                } 
+                    using (CsvWriter csv = new CsvWriter(sw, CultureInfo.CurrentCulture))
+                    {
+                        csv.WriteRecords(resultdata);
+                    }
+                }
+                GoSpin = false;
+                return LogPath;
+            }
+            catch (Exception ex)
+            {
+                GoSpin = false;
+                MessageBox.Show("Hash calculation task failed with exception.\r\n" + ex.StackTrace,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error,MessageBoxDefaultButton.Button1);
+                return string.Empty;
             }
         }
+
+        static void ResultComparison()
+        {
+
+        }
+        static string FilenameGenerator(string folder, string fileName, int maxAttempts = 1024)
+        {
+            var fileBase = Path.GetFileNameWithoutExtension(fileName);
+            var ext = Path.GetExtension(fileName);
+
+            // Build hash set of filenames for performance
+            var files = new HashSet<string>(Directory.GetFiles(folder));
+
+            for (var index = 0; index < maxAttempts; index++)
+            {
+                // First try with the original filename, else try incrementally adding an index
+                var name = (index == 0)
+                    ? fileName
+                    : string.Format("{0} ({1}){2}", fileBase, index, ext);
+
+                // Check if exists
+                var fullPath = Path.Combine(folder, name);
+                if (files.Contains(fullPath))
+                    continue;
+
+                // Try to create the file
+                try
+                {
+                    return fullPath;
+                }
+                catch (DirectoryNotFoundException) { throw; }
+                catch (DriveNotFoundException) { throw; }
+                catch (IOException)
+                {
+                    // Will occur if another thread created a file with this name since we created the HashSet.
+                    // Ignore this and just try with the next filename.
+                }
+            }
+            throw new Exception("Could not create unique filename in " + maxAttempts + " attempts");
+        }
+
     }
     public class FileData
     {
